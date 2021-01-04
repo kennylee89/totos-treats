1f88188628df6611e23bc07af5ca29d5a327267f
kennylee <kennyhojinlee@gmail.com>
2021-01-03 20:58:00 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Toto's Treats
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Woof Woof!
         </a>
       </header>
     </div>
